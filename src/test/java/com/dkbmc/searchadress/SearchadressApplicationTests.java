db2b8bbe85f5c110a5b01b44ca06e015a27643e6
billiamchoi <billiamchoi4u@gmail.com>
2023-05-12 16:29:57 +0900

    기본 화면 구현

---
+++ src/test/java/com/dkbmc/searchadress/SearchadressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkbmc.searchadress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchadressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
